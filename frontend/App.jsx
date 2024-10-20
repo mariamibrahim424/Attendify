feed66155d47b8821976baeb60cf789fe2428667
Mariam Ibrahim <mibrahi3@ucsc.edu>
2024-10-19 23:07:53 -0700

    hello world

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
+import {Text, SafeAreaView} from 'react-native';
 
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello, World!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;

3efcfc40f8a7f0534a0d653df1741c63306c2c1b
Mariam Ibrahim <mibrahi3@ucsc.edu>
2024-10-26 22:58:20 -0700

    Revert "firebase config"
    This reverts commit 4ef519c97e036eaf0e90ba4834c4c3946127b3ce.

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,17 +1,11 @@
+import React from 'react';
 import {Text, SafeAreaView} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Login from './src/screens/Login';
-
-const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
-        <Stack.Screen name='login' component={Login} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView>
+      <Text>Hello, World!</Text>
+    </SafeAreaView>
   );
 };
 
